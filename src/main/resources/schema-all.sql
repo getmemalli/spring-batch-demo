322acf43dfd5b0863fff7320b8126195b09c8b46
Pankaj kumar <pankaj.kumar@razorpay.com>
2018-11-26 15:58:35 +0530

    adding updated schema

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -1,7 +1,10 @@
---DROP TABLE people IF EXISTS;
---
---CREATE TABLE person  (
---    person_id BIGINT NOT NULL PRIMARY KEY,
---    first_name VARCHAR(20),
---    last_name VARCHAR(20)
---);
+DROP TABLE people IF EXISTS;
+
+
+CREATE TABLE `person` (
+  `person_id` bigint(20) NOT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `status` varchar(20) DEFAULT 'UNPROCESSED',
+  PRIMARY KEY (`person_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
